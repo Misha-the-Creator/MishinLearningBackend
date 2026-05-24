ea5e14135c34ba23500c90fc6a5d96dfd44a8c8e
Mikhail Zuev <mikhailneue@gmail.com>
2026-05-24 15:13:01 +0300

    add db configuration

--- src/core/config.py
+++ src/core/config.py
@@ -1,17 +1,22 @@
 from pydantic_settings import BaseSettings
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 
 class RunCongif(BaseModel):
     host: str = '0.0.0.0'
-    port: int = 8000
+    port: int = 8080
     reload: bool = True
 
 class ApiPrefix(BaseModel):
     prefix: str = '/api'
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False 
+    pool_size: int = 25
+
 class Setting(BaseSettings):
     run: RunCongif = RunCongif()
     api: ApiPrefix = ApiPrefix()
-    db_url: str
+    db: DatabaseConfig = DatabaseConfig() 
 
 settings = Setting()
