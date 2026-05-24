befc95a3bdb6abeb79112e734baea555bdb09555
Mikhail Zuev <mikhailneue@gmail.com>
2026-05-24 17:16:36 +0300

    change path in alembic env

--- alembic/env.py
+++ alembic/env.py
@@ -4,9 +4,9 @@ from logging.config import fileConfig
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-from core.models.base import Base
+from src.core.models.base import Base
 from alembic import context
-from core.config import settings
+from src.core.config import settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
